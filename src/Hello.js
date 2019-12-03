c102b7bf67ccf40cc12854a70505f5b97786faf8
ninjasul <ninjasul@gmail.com>
2019-12-04 01:48:00 +0900

    props

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-function Hello() {
-    return <div>안녕하세요</div>;
+/*
+function Hello(props) {
+    console.log(props);
+    return <div style={{
+        color: props.color
+    }}>안녕하세요 {props.name}</div>;
+}
+*/
+
+function Hello({ color, name }) {
+    return <div style={{
+        color
+    }}>안녕하세요 {name}</div>;
+}
+
+Hello.defaultProps = {
+    name: '이름없음'
 }
 
 export default Hello;
