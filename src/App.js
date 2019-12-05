b125575614d7a60ea7ba854d3bbb4c68b9877523
ninjasul <ninjasul@gmail.com>
2019-12-06 00:19:20 +0900

    배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+
+    // 불변이므로 복제한 후 name, value 쌍을 입력함.
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -18,17 +36,41 @@ function App() {
       username: 'ka2377',
       email: 'ka2377@naver.com'
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+
+    // 배열에 추가할 경우에는 스프레드 연산자(...)나 concat을 사용해야 함.
+    // push, splice는 사용하면 안됨.
+    //setUsers([...users, user]);
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: '',
+      email: '',
+    });
+
     console.log(nextId.current);
     nextId.current += 1;
   };
 
   return (
-     <UserList users={users}/>
+    <>
+      <CreateUser
+         username={username}
+         email={email}
+         onChange={onChange}
+         onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
