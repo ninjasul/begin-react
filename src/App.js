498b2199783ba0ac383120aae7f33aa52d67c689
ninjasul <ninjasul@gmail.com>
2019-12-12 23:18:04 +0900

    immer 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useReducer, useMemo, useCallback, createContext } from 'react';
+import produce from 'immer';
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
 import useInputs from "./useInputs";
@@ -34,23 +35,39 @@ const initialState = {
 function reducer(state, action) {
   switch(action.type) {
     case 'CREATE_USER':
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
+/*
       return {
         inputs: initialState.inputs,
         users: state.users.concat(action.user)
       };
+*/
     case 'TOGGLE_USER':
-      return {
+      /*return {
         ...state,
         users: state.users.map(user =>
             user.id === action.id
             ? { ...user, active: !user.active } : user
         )
-      };
+      };*/
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
+
     case 'REMOVE_USER':
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
+/*
       return {
         ...state,
         users: state.users.filter(user => user.id !== action.id)
       };
+*/
     default:
       throw new Error('Unhandled action');
   }
